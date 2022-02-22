66760e6278743940d0dfeff8668cbaf637587720
Eric O. Jonathan <ericojonathan@hotmail.com>
2022-02-22 20:49:16 +0700

    git adding sync communication service to API_2 and API_Template

--- src/api_template/index.js
+++ src/api_template/index.js
@@ -310,6 +310,60 @@ app.get('/employees', (req, res) => {
     })
 });
 
+app.get('/employee_isc', (req, res) => {
+    //cache will be based on an API Key. 
+    //access without API key will be rejected
+    console.log("[/employee_isc]")
+    let key_recv_primary = req.body.api_key_primary;    
+    
+    if(key_recv_primary === undefined || key_recv_primary === null || key_recv_primary =='' || key_recv_primary != api_key) {
+        res.status(401).send("Unauthorized access! Please use your API Key to use this service.\n")
+        return;
+    }
+
+    let api_key_isc = config.get('isc.api_key');
+    let key_recv_isc = req.body.api_key_secondary;
+
+    if(key_recv_isc === undefined || key_recv_isc === null || key_recv_isc =='' || key_recv_isc != api_key_isc) {
+        res.status(401).send("Unauthorized access! Please use your secondary API Key to use this inter service communicate service.")
+        return;
+    }
+
+    let query_length = Object.keys(req.body).length;
+    if(query_length > 3) {
+        res.status(400).send("<h1>Query error!</h1><p>For now, query is based on <b>one</b> key=value only.</p><p>E.g. job_title=Manager. job_title=Manager&email=example@example.com will produce this query error.</p>")
+        return;
+    }
+
+    //Values from config file
+    let isc_host = config.get("isc.host");
+    let isc_port = config.get("isc.port");
+    let url = `http://${isc_host}:${isc_port}/employee?`;
+
+    if(query_length > 1) {
+        //iterates
+        for(key in req.body) {
+            if(key =="api_key_primary" || key =="api_key_secondary") {                                
+                continue;
+            }            
+            url += `${key}=${req.body[key]}&`;
+        }        
+    }
+
+    url += `api_key=${api_key_isc}`;    
+
+    try {
+        let sync_res = sync_request('GET', url);    
+        let data = JSON.parse(sync_res.getBody('utf8'));
+        data.job_title = decrypt(data.job_title);
+        data.email_address = decrypt(data.email_address);
+        data.firstName_LastName = decrypt(data.firstName_LastName);    
+        res.json(data);        
+    } catch {
+        res.status(400).send("<h1>Bad Request</h1>")
+    }    
+});
+
 //Inter-service communication (isc) using sync and async communication
 app.get('/employees_isc', async (req, res) => {
     //cache will be based on an API Key. 
